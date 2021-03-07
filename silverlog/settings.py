486dbc07aa3f232bcb97cf3796c69ac620e57bc9
rimvydask <rimvydas.kanapka@sandas.lt>
2021-03-07 15:54:12 +0200

    [UPD] Set up PostgreSQL database instead of SQLite

--- silverlog/settings.py
+++ silverlog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'silverlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'silverlog',
+        'USER': 'silverlog_admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
