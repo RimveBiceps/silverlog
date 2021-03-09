7dc400fa3d6cde3bb94ef25201622f0aa8ab6042
rimvydask <rimvydas.kanapka@sandas.lt>
2021-03-09 15:42:02 +0200

    [UPD] Using auto farmatting tool autopep8 - Ctrl+Shift+I

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path("", views.handle_home, name = "home"),
-    path('register', views.handle_register, name = "register"),
-    path("login", views.handle_login, name = "login"),
-    path("logout", views.handle_logout, name= "logout"),
+    path("", views.handle_home, name="home"),
+    path('register', views.handle_register, name="register"),
+    path("login", views.handle_login, name="login"),
+    path("logout", views.handle_logout, name="logout"),
 ]
